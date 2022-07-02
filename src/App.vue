e8d1e62ec0d61bd62b308c6e854dee627b03b404
sandis inc <sandis@inc.com>
2022-07-02 07:13:03 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <SimpleComponent @add-task="addTask" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import SimpleComponent from "@/components/SimpleComponent.vue";
+
+</script>
+
+<style lang = "scss" scoped>
+.container {
+  max-width: 650px;
+  width: 100%;
+  padding: 40px 16px;
+  margin-inline: auto;
+}
+
+@media screen and (min-width: 525px) {
+  .container {
+    padding: 64px 32px;
+  }
+}
+</style>
---
+++ src/components/SimpleComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <form class="simpleForm" @submit.prevent="simpleForm">
+        <input type="text" class="simpleInput" v-model="simpleModel"/>
+        <button class="simple-btn">Button</button>
+    </form>
+</template>
+
+<script lang="ts" setup>
+import {defineEmits, ref} from 'vue';
+
+const emit = defineEmits<{
+    (e: 'add-task', value: string): void;
+}>();
+
+const simpleModel = ref<string>('');
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/main';
+.simpleForm {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    color: $c-success;
+}
+
+.simpleInput {
+    padding: 12px 16px;
+    border: 1px solid #000;
+    font-size: 20px;
+}
+
+.simple-btn {
+    padding: 12px 24px;
+    background-color: #000;
+    border: 1px solid #000;
+    font-size: 20px;
+    font-weight: 500;
+    color: #fff;
+}
+
+@media screen and (min-width: 525px) {
+    .simpleForm {
+        flex-direction: row;
+    }
+
+    .simpleInput {
+        flex-grow: 1;
+    }
+}
+</style>
